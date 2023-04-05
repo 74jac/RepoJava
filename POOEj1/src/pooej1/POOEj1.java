ada2cad0d43247903ef1bb42e981c07d4e9ab536
“Agustin <agustincordero74@gmail.com>
2023-04-05 14:17:40 -0300

    segundo commit

--- POOEj1/src/pooej1/POOEj1.java
+++ POOEj1/src/pooej1/POOEj1.java
@@ -3,19 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package encuentro_6;
+package pooej1;
 
 /**
  *
  * @author JoséAgustín
  */
-public class Encuentro_6 {
+public class POOEj1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO co  de application logic here
+        
+        
     }
     
 }
